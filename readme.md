bf3c5146a070366d0dba2f69eacc284dc4fd4abd
AlexORocha <rocha.alex.oliveira@gmail.com>
2023-07-01 18:17:06 -0300

    commit da aula 02

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Descrição do Projeto
+
+Esse repositório contém um projeto desenvolvido em JavaScript utilizando o NodeJS com o Framework Express para a criação de um conjunto de APIs no padrão REST utilizando o protocolo HTTP para realizar açÕes de CRUD (create, read, update, delete) em um banco de dados NoSQL - MongoDB. Esse projeto foi criado a partir do curso de NodeJS da Alura.
+
+## Dependências
+
+NodeJS - v18.16.0
+Express - v4.18.2
+Nodemon - 2.0.22
+
+## Documentação
+
+Em desenvolvimento...
--- server.js
+++ server.js
@@ -1,19 +1,6 @@
-const http = require("http")
-const port = 3000;
+import app from './src/app.js'
+const port = process.env.PORT || 3000;
 
-const rotas = {
-    '/': 'Curso de Node',
-    '/livros': 'Página de Livros',
-    '/autores': 'Listagem de Autores',
-    '/sobre': 'Info sobre o Projeto'
-}
-
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(rotas[req.url]);
-})
-
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Servidor executando em http://localhost:${port}`)
 })
