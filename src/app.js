166a0d0fe0aa8f88372f842d07d560b466fe5b36
AlexORocha <rocha.alex.oliveira@gmail.com>
2023-07-01 21:45:58 -0300

    aprimorando o projeto - MVC (Model View Controller)

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "./config/dbConnect.js";
 import livros from "./models/livro.js";
+import routes from "./routes/index.js";
 
 db.on("error", console.log.bind(console, 'Erro de conexão com o Banco de Dados'))
 db.once("open", () => {
@@ -10,49 +11,6 @@ db.once("open", () => {
 const app = express();
 app.use(express.json());
 
-
-app.get('', (req, res) => {
-    res.status(200).send('Curso de NodeJS');
-})
-
-
-app.get('/livros', async (req, res) => {
-    try {
-      const resultado = await livros.find();
-      res.status(200).json(resultado);
-    } catch (error) {
-      console.error('Erro ao obter os livros:', error);
-      res.status(500).json({ error: 'Erro ao obter os livros' });
-    }
-});
-
-
-app.get('/livros/:id', (req, res) => {
-    let index = buscaLivro(req.params.id);
-    res.status(200).json(livros[index])
-})
-
-app.post('/livros', (req, res) => {
-    livros.push(req.body);
-    res.status(201).send('Livro cadastrado com Sucesso!')
-})
-
-app.put('/livros/:id', (req, res) => {
-    let index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo;
-    res.status(200).json(livros)
-})
-
-app.delete('/livros/:id', (req, res) => {
-    let {id} = req.params;
-    let index = buscaLivro(id);
-    livros.splice(index, 1);
-    res.status(204).send(`O livro ${id} foi excluído com sucesso`)
-})
-
-
-function buscaLivro(id){
-    return livros.findIndex(livro => livro.id == id)
-}
+routes(app);
 
 export default app  
---
+++ src/routes/livrosRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import LivroController from "../controllers/livrosController.js";
+
+const router = express.Router();
+
+router
+    .post("/livros", LivroController.cadastrarLivros)
+    .get("/livros", LivroController.listarLivros)
+    .get("/livros/:id", LivroController.listarLivroEspecifico)    
+    .put("/livros/:id", LivroController.atualizarLivro)
+    .delete("/livros/:id", LivroController.deletarLivro)
+    
+export default router;
